e714f7c264474d9c3803259c1381c9fc7cb543ad
Matias Netto <matiasnetto03@gmail.com>
2023-05-02 02:22:34 -0300

    Navbar styles rework

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,10 +9,23 @@ import { BASE_URL } from 'src/config';
 })
 export class NavbarComponent implements OnInit {
   data: IAboutMe | null = null;
+  menuOpened: boolean = false;
+
+  links = [
+    { link: 'about', title: 'About' },
+    { link: 'experience', title: 'Experience' },
+    { link: 'education', title: 'Education' },
+    { link: 'skills', title: 'Habilidades' },
+    { link: 'projects', title: 'Projects' },
+  ];
 
   constructor(private service: PortfolioService) {}
 
   ngOnInit(): void {
     this.service.getAboutMe().subscribe((data) => (this.data = data));
+  }
+
+  public toggleMenu() {
+    this.menuOpened = !this.menuOpened;
   }
 }
--- src/app/services/portfolio.service.ts
+++ src/app/services/portfolio.service.ts
@@ -68,7 +68,7 @@ export interface IAboutMe {
   providedIn: 'root',
 })
 export class PortfolioService {
-  private BACKEND_URL: string = 'http://localhost:3000/api';
+  private BACKEND_URL: string = 'http://192.168.1.40:3000/api';
 
   constructor(private http: HttpClient) {}
 
